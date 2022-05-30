4132b82d0252ab4b6c2102bd459c1a57a3946c55
vishram-shetty <vishramshetty07@gmail.com>
2022-05-30 15:05:50 +0530

    Form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./styles/main.css";
 const HomePage = React.lazy(() => import("./Components/Pages/HomePage"));
 const cartPage = React.lazy(() => import("./Components/BookCart/cartPage"));
 const CourseDesc = React.lazy(() => import("./Components/CourseDesc/CourseDesc"));
+const Form = React.lazy(() => import("./Components/Form/form"));
 
 const App = () => (
   <Router history={history}>
@@ -15,6 +16,7 @@ const App = () => (
         <Route exact path="/" component={HomePage} />
         <Route exact path="/cart" component={cartPage} />
         <Route exact path="/detailsPage" component={CourseDesc} />
+        <Route exact path="/form" component={Form} />
       </Switch>
     </Suspense>
   </Router>
